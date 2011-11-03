f2deeea71823a8c2e86c4560925cb8fec3da8277
Ravi Shakya <ravi.shakya@gmail.com>
2011-11-03 16:46:35 +0530

    implemented Feed forward neural network with given parmaters

--- octave/mlclass-ex3/predict.m
+++ octave/mlclass-ex3/predict.m
@@ -21,8 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add additional 1
+X = [ones(m, 1), X];
 
-
+a2 = sigmoid(X* Theta1');
+a2 = [ones(m, 1), a2];
+[temp, p] = max(sigmoid(a2 * Theta2'), [], 2);
 
 
 
