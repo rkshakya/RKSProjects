493e90bad788634e3b98321f7eabe11fce650ae9
Ravi Shakya <ravi.shakya@gmail.com>
2015-12-04 18:04:13 +0545

    IOS apps

--- java_codes/BulkMetadataExport.java
+++ java_codes/BulkMetadataExport.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 /*******************************************************************************
  * BulkMetadataExport.java - <comment>
